d4cf02219746d07df1cb780a68b4ed9607097537
Andres Puello <apuello1025@gmail.com>
2021-12-01 18:14:52 -0500

    Redux Counter

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { createStore } from "redux";
+
+const initialState = { counter: 0, show: true };
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increment") {
+    return {
+      ...state,
+      counter: state.counter + (action.payload || 1),
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      ...state,
+      counter: state.counter - (action.payload || 1),
+    };
+  }
+  if (action.type === "show") {
+    return {
+      ...state,
+      show: !state.show,
+    };
+  }
+  if (action.type === "reset") {
+    return {
+      ...state,
+      counter: 0,
+    };
+  }
+
+  return state;
+};
+export const store = createStore(counterReducer);
